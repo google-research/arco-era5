f3ca6a26603ba703b081809e0791f4b612dd5c69
dabhicusp <sahildabhi@infocusp.com>
2023-08-10 12:15:58 +0000

    Necessary changes were done.

--- raw/gcs_data_consistency_checker.py
+++ raw/gcs_data_consistency_checker.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+import fsspec
+import pandas as pd
 import typing as t
-from datetime import datetime, timedelta
-from google.cloud import storage
 
 BUCKET = "gcp-public-data-arco-era5"
 START_YEAR = 1979
@@ -22,131 +22,87 @@ END_YEAR = 2022
 
 
 def list_files_in_directory(bucket_name: str, directory_prefix: str) -> t.List[str]:
-    """Lists files in a specific directory within a Google Cloud Storage bucket."""
-    client = storage.Client()
-    bucket = client.get_bucket(bucket_name)
-    return [blob.name for blob in bucket.list_blobs(prefix=directory_prefix)]
+    """List files in a directory of a cloud storage bucket using fsspec.
+
+    Args:
+        bucket_name (str): The name of the cloud storage bucket.
+        directory_prefix (str): The prefix of the directory.
+
+    Returns:
+        List[str]: A list of file names in the directory.
+    """
+    fs = fsspec.filesystem('gs')
+    files = fs.ls(f'{bucket_name}/{directory_prefix}')
+    return [file[len(bucket_name) + 1:] for file in files]
 
 
 def find_extra_files(directory_files: t.List[str], desired_files: t.List[str]
                      ) -> t.List[str]:
-    """Returns a list of files that are in the directory but not in the desired list."""
-    return [file for file in directory_files if file not in desired_files]
+    """Returns a list of files that are in the directory but not in the desired list.
+
+    Args:
+        directory_files (List[str]): List of files in the directory.
+        desired_files (List[str]): List of desired file names.
+
+    Returns:
+        List[str]: A list of file names that are extra in the directory.
+    """
+    return list(set(directory_files) - set(desired_files))
 
 
-def generate_daily_file_names(year: int) -> t.List[str]:
-    """Generates a list of daily file names for a given year."""
+def generate_daily_file_names(year: int, chunks: t.List) -> t.List[str]:
+    """Generate a list of daily file names for a given year.
+
+    Args:
+        year (int): The year for which to generate file names.
+        chunks (List):  A list of the daily files chunks.
+    Returns:
+        List[str]: A list of generated daily file names.
+    """
     generated_files = []
-    current_date = datetime(year, 1, 1)
-    end_date = datetime(year + 1, 1, 1)
-    delta = timedelta(days=1)
+    start_date = pd.Timestamp(year, 1, 1)
+    end_date = pd.Timestamp(year + 1, 1, 1)
+    date_range = pd.date_range(start_date, end_date, freq='D')
 
-    while current_date < end_date:
-        formatted_date = current_date.strftime('%Y%m%d')
-        generated_files.append(
-            f'raw/ERA5GRIB/HRES/Daily/{year}/{formatted_date}_hres_dve.grb2')
-        generated_files.append(
-            f'raw/ERA5GRIB/HRES/Daily/{year}/{formatted_date}_hres_o3q.grb2')
-        generated_files.append(
-            f'raw/ERA5GRIB/HRES/Daily/{year}/{formatted_date}_hres_qrqs.grb2')
-        generated_files.append(
-            f'raw/ERA5GRIB/HRES/Daily/{year}/{formatted_date}_hres_tw.grb2')
-        generated_files.append(
-            f'raw/ERA5GRIB/HRES/Daily/{year}/{formatted_date}_hres_tuvw.grb2')
-        current_date += delta
+    for date in date_range:
+        formatted_date = date.strftime('%Y%m%d')
+        for chunk in chunks:
+            generated_files.append(
+                f'raw/ERA5GRIB/HRES/Daily/{year}/{formatted_date}_hres_{chunk}.grb2')
     return generated_files
 
 
-def generate_monthly_file_names(year: int) -> t.List[str]:
+def generate_monthly_file_names(year: int,
+                                monthly_chunks: t.List, monthly_chunks_pcp: t.List,
+                                monthly_chunks_soil: t.List) -> t.List[str]:
     """Generates a list of desired monthly file names for a given year."""
     monthly_dates = [f"{year}{month:02d}" for month in range(1, 13)]
-    desired_files_monthly = []
+    generated_files = []
 
     for month in monthly_dates:
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_cape.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_cisst.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_lnsp.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_cp.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_crr.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_csf.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_csfr.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_es.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_lsf.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_lsp.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_lspf.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_lsrr.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_lssfr.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_ptype.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_rsn.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_sd.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_sf.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_smlt.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_tp.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_rad.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_sfc.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_istl1.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_istl2.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_istl3.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_istl4.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_stl1.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_stl2.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_stl3.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_stl4.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_swvl1.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_swvl2.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_swvl3.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_swvl4.grib')
-        desired_files_monthly.append(
+        for chunk in monthly_chunks:
+            generated_files.append(
+                f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_{chunk}.grb2')
+        for chunk in monthly_chunks_pcp:
+            generated_files.append(
+                f"raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_pcp.grb2_surface_{chunk}.grib")
+        for chunk in monthly_chunks_soil:
+            generated_files.append(
+                f"raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_depthBelowLandLayer_{chunk}.grib")
+        generated_files.append(
             f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_soil.grb2_surface_tsn.grib')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_tcol.grb2')
-        desired_files_monthly.append(
-            f'raw/ERA5GRIB/HRES/Month/{year}/{month}_hres_zs.grb2')
-
-    return desired_files_monthly
+    return generated_files
 
 
 def validate_and_report_missing_files(bucket_name: str, directory_prefix: str,
                                       desired_files: t.List[str]):
-    """Validates and reports missing files in the specified directory for a given
-    year."""
+    """Validates and reports missing files in the specified directory for a given year.
+
+    Args:
+        bucket_name (str): The name of the cloud storage bucket.
+        directory_prefix (str): The prefix of the directory.
+        desired_files (List[str]): List of desired file names.
+    """
     try:
         actual_files = list_files_in_directory(bucket_name, directory_prefix)
         extra_files = find_extra_files(actual_files, desired_files)
@@ -159,6 +115,8 @@ def validate_and_report_missing_files(bucket_name: str, directory_prefix: str,
             print(f"Extra files in {directory_prefix}:")
             for file in extra_files[1:]:
                 print(file)
+        else:
+            print(f"There is no extra files in {directory_prefix}.")
     except Exception as e:
         print(f"An error occurred: {e}")
 
@@ -166,14 +124,22 @@ def validate_and_report_missing_files(bucket_name: str, directory_prefix: str,
 if __name__ == '__main__':
     for year in range(START_YEAR, END_YEAR + 1):
         # Process daily files
+        daily_chunks = ['dve', 'o3q', 'qrqs', 'tw', 'tuvw']
         directory_prefix_daily = f"raw/ERA5GRIB/HRES/Daily/{year}/"
-        desired_files_daily = generate_daily_file_names(year)
+        desired_files_daily = generate_daily_file_names(year, daily_chunks)
         validate_and_report_missing_files(BUCKET, directory_prefix_daily,
                                           desired_files_daily)
 
         # Process monthly files
+        monthly_chunks = ['cape', 'cisst', 'lnsp', 'pcp', 'rad', 'sfc', 'soil', 'tcol',
+                          'zs']
+        monthly_chunks_pcp = ['cp', 'crr', 'csf', 'csfr', 'es', 'lsf', 'lsp', 'lspf',
+                              'lsrr', 'lssfr', 'ptype', 'rsn', 'sd', 'sf', 'smlt', 'tp']
+        monthly_chunks_soil = ['istl1', 'istl2', 'istl3', 'istl4', 'stl1', 'stl2',
+                               'stl3', 'stl4', 'swvl1', 'swvl2', 'swvl3', 'swvl4']
         directory_prefix_monthly = f"raw/ERA5GRIB/HRES/Month/{year}/"
-        desired_files_monthly = generate_monthly_file_names(year)
+        desired_files_monthly = generate_monthly_file_names(
+            year, monthly_chunks, monthly_chunks_pcp, monthly_chunks_soil)
         validate_and_report_missing_files(BUCKET, directory_prefix_monthly,
                                           desired_files_monthly)
 
