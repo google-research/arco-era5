db43c7ff275f830e486b7b0a5c27b2250037bf61
Alex Merose <alxr@google.com>
2023-08-29 09:46:55 -0700

    Adding source code to produce the `ar/` version of this dataset. (#46)
    Co-authored-by: dabhicusp <sahildabhi@infocusp.com>

---
+++ src/arco_era5/source_data.py
@@ -0,0 +1,272 @@
+"""Information and reading utils for the ERA 5 dataset."""
+
+__author__ = 'Matthew Willson, Alvaro Sanchez, Peter Battaglia, Stephan Hoyer, Stephan Rasp'
+
+import argparse
+import fsspec
+import immutabledict
+
+import pathlib
+import xarray
+
+import numpy as np
+import pandas as pd
+import typing as t
+import xarray as xr
+
+TIME_RESOLUTION_HOURS = 1
+
+GCP_DIRECTORY = "gs://gcp-public-data-arco-era5/raw"
+
+STATIC_SUBDIR_TEMPLATE = "date-variable-static/2021/12/31/{variable}/static.nc"
+
+SINGLE_LEVEL_SUBDIR_TEMPLATE = (
+    "date-variable-single_level/{year}/{month:02d}/"
+    "{day:02d}/{variable}/surface.nc")
+
+MULTILEVEL_SUBDIR_TEMPLATE = (
+    "date-variable-pressure_level/{year}/{month:02d}/"
+    "{day:02d}/{variable}/{pressure_level}.nc")
+
+STATIC_VARIABLES = (
+    "type_of_low_vegetation",
+    "type_of_high_vegetation",
+    "standard_deviation_of_orography",
+    "standard_deviation_of_filtered_subgrid_orography",
+    "soil_type",
+    "slope_of_sub_gridscale_orography",
+    "low_vegetation_cover",
+    "land_sea_mask",
+    "lake_depth",
+    "lake_cover",
+    "high_vegetation_cover",
+    "geopotential_at_surface",
+    "anisotropy_of_sub_gridscale_orography",
+    "angle_of_sub_gridscale_orography",
+)
+
+# TODO(alvarosg): Add more variables.
+SINGLE_LEVEL_VARIABLES = (
+    "total_precipitation",
+    "total_column_water_vapour",
+    "total_cloud_cover",
+    "toa_incident_solar_radiation",
+    "surface_pressure",
+    "sea_surface_temperature",
+    "sea_ice_cover",
+    "mean_sea_level_pressure",
+    "2m_temperature",
+    "10m_v_component_of_wind",
+    "10m_u_component_of_wind",
+)
+
+MULTILEVEL_VARIABLES = (
+    "geopotential",
+    "specific_humidity",
+    "temperature",
+    "u_component_of_wind",
+    "v_component_of_wind",
+    "vertical_velocity",
+)
+
+# Variables that correspond to an integration over a `TIME_RESOLUTION_HOURS`
+# interval, rather than an instantaneous sample in time.
+# As per:
+# https://confluence.ecmwf.int/pages/viewpage.action?pageId=197702790
+# "ERA5 reanalysis (hourly data): accumulations are over the hour
+# (the accumulation/processing "period) ending at the validity date/time"
+# TODO(alvarosg): Verify somehow that it is true that these variables are indeed
+# integrated for 1 hour, and not longer, because it is a bit suspicious that
+# we don't have values for these variables until 1959-01-01 07:00:00, while
+# for all other variables we have values since 1959-01-01 00:00:00, which may
+# indicate at 6-8h accumulation.
+CUMULATIVE_VARIABLES = ("total_precipitation",
+                        "toa_incident_solar_radiation")
+
+PRESSURE_LEVELS_GROUPS = immutabledict.immutabledict({
+    "weatherbench_13":
+        (50, 100, 150, 200, 250, 300, 400, 500, 600, 700, 850, 925, 1000),
+    "full_37": (1, 2, 3, 5, 7, 10, 20, 30, 50, 70, 100, 125, 150, 175, 200, 225,
+                250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 775, 800,
+                825, 850, 875, 900, 925, 950, 975, 1000)
+})
+
+# ERA5 uses "geopotential" for file names for both the static "geopotential"
+# feature at surface, and the dynamic multilevel "geopotential". To avoid this
+# clash, we will always rename the static one to "geopotential_at_surface".
+_VARIABLE_TO_ERA5_FILE_NAME = {
+    "geopotential_at_surface": "geopotential"
+}
+
+
+def _read_nc_dataset(gpath_file):
+    """Read the .nc dataset from disk."""
+    path = str(gpath_file).replace('gs:/', 'gs://')
+    with fsspec.open(path, mode="rb") as fid:
+        dataset = xarray.open_dataset(fid, engine="scipy", cache=False)
+    # All dataset have a single data array in them, so we just return the array.
+    assert len(dataset) == 1
+    dataarray = next(iter(dataset.values()))
+    if "expver" in dataarray.coords:
+        # Recent ERA5 downloads (within 2-3 months old) can include data from ERA5T,
+        # which is the temporary release of ERA5, which has not been validated.
+        # For such recent data, all instantaneous variables are from ERA5T, while
+        # accumulated variables can be a mix of ERA5 and ERA5T, because the earliest
+        # times of the accumulation window spans back from the range covered by
+        # ERA5T into the range covered by ERA5.
+        # Such NetCDF files indicate which ERA5 vs ERA5T with the `expver` axis,
+        # where `expver=1` means ERA5 and `expver=5` means ERA5T. For ERA5 times,
+        # NaNs are present in the `expver=5` slice, and for ERA5T times, NaNs are
+        # present in the `expver=1` slice.
+        # We combine and remove the `expver` axes using the below command.
+        # See: https://confluence.ecmwf.int/pages/viewpage.action?pageId=173385064
+        # and: https://confluence.ecmwf.int/display/CKB/ERA5%3A+data+documentation#ERA5:datadocumentation-Dataupdatefrequency  # pylint: disable=line-too-long
+        # for further details.
+
+        all_dims_except_time = tuple(set(dataarray.dims) - {"time"})
+        # Should have only trailing nans.
+        a = dataarray.sel(expver=1).isnull().any(dim=all_dims_except_time)
+        # Should having only leading nans.
+        b = dataarray.sel(expver=5).isnull().any(dim=all_dims_except_time)
+        disjoint_nans = bool(next(iter((a ^ b).all().data_vars.values())))
+        assert disjoint_nans, "The nans are not disjoint in expver=1 vs 5"
+        dataarray = dataarray.sel(expver=1).combine_first(dataarray.sel(expver=5))
+    return dataarray
+
+
+def read_static_vars(variables=STATIC_VARIABLES, root_path=GCP_DIRECTORY):
+    """xarray.Dataset with static variables for single level data from nc files."""
+    root_path = pathlib.Path(root_path)
+    output = {}
+    for variable in variables:
+        if variable in _VARIABLE_TO_ERA5_FILE_NAME:
+            era5_variable = _VARIABLE_TO_ERA5_FILE_NAME[variable]
+        else:
+            era5_variable = variable
+        relative_path = STATIC_SUBDIR_TEMPLATE.format(variable=era5_variable)
+        output[variable] = _read_nc_dataset(root_path / relative_path)
+    return xarray.Dataset(output)
+
+
+def read_single_level_vars(year, month, day, variables=SINGLE_LEVEL_VARIABLES,
+                           root_path=GCP_DIRECTORY):
+    """xarray.Dataset with variables for singel level data from nc files."""
+    root_path = pathlib.Path(root_path)
+    output = {}
+    for variable in variables:
+        relative_path = SINGLE_LEVEL_SUBDIR_TEMPLATE.format(
+            year=year, month=month, day=day, variable=variable)
+        output[variable] = _read_nc_dataset(root_path / relative_path)
+    return xarray.Dataset(output)
+
+
+def read_multilevel_vars(year,
+                         month,
+                         day,
+                         variables=MULTILEVEL_VARIABLES,
+                         pressure_levels=PRESSURE_LEVELS_GROUPS["full_37"],
+                         root_path=GCP_DIRECTORY):
+    """xarray.Dataset with variables for all levels from nc files."""
+    root_path = pathlib.Path(root_path)
+    output = {}
+    for variable in variables:
+        pressure_data = []
+        for pressure_level in pressure_levels:
+            relative_path = MULTILEVEL_SUBDIR_TEMPLATE.format(
+                year=year, month=month, day=day, variable=variable,
+                pressure_level=pressure_level)
+            single_level_data_array = _read_nc_dataset(root_path / relative_path)
+            single_level_data_array.coords["level"] = pressure_level
+            pressure_data.append(
+                single_level_data_array.expand_dims(dim="level", axis=1))
+        output[variable] = xarray.concat(pressure_data, dim="level")
+    return xarray.Dataset(output)
+
+
+def get_var_attrs_dict(root_path=GCP_DIRECTORY):
+    """Returns attributes for all variables."""
+    root_path = pathlib.Path(root_path)
+
+    # The variable attributes should be independent of the date chosen here
+    # so we just choose any date.
+    year = 2021
+    month = 1
+    day = 1
+    pressure_level = PRESSURE_LEVELS_GROUPS["full_37"][-1]
+
+    # TODO(alvarosg): Store a version of this somewhere so we don't actually
+    # have to download the data files to get this into.
+    var_attrs_dict = {}
+    for variables, template, rename_dict in [
+        (STATIC_VARIABLES, STATIC_SUBDIR_TEMPLATE, _VARIABLE_TO_ERA5_FILE_NAME),
+        (SINGLE_LEVEL_VARIABLES, SINGLE_LEVEL_SUBDIR_TEMPLATE, {}),
+        (MULTILEVEL_VARIABLES, MULTILEVEL_SUBDIR_TEMPLATE, {}),
+    ]:
+        for variable in variables:
+            if variable in rename_dict:
+                era5_variable = rename_dict[variable]
+            else:
+                era5_variable = variable
+            relative_path = template.format(
+                year=year, month=month, day=day, variable=era5_variable,
+                pressure_level=pressure_level)
+            data_array = _read_nc_dataset(root_path / relative_path)
+            data_array.attrs["short_name"] = data_array.name
+            var_attrs_dict[variable] = data_array.attrs
+    return var_attrs_dict
+
+
+def daily_date_iterator(start_date: str, end_date: str
+                        ) -> t.Iterable[t.Tuple[int, int, int]]:
+    """Iterates all (year, month, day) tuples between start_date and end_date."""
+    date_range = pd.date_range(start=start_date, end=end_date, inclusive='left')
+    for date in date_range:
+        yield date.year, date.month, date.day
+
+
+def align_coordinates(dataset: xr.Dataset) -> xr.Dataset:
+    """Align coordinates of per-variable datasets prior to consolidation."""
+
+    # It's possible to have coordinate metadata for coordinates which aren't
+    # actually used as dimensions of any variables (called 'non-index'
+    # coordinates), and some of the source NetCDF files use these, e.g. a scalar
+    # 'height' coordinate (= 2.0) in the NetCDF files for the 2-meter temperature
+    # variable tas. We remove these, for simplicity and since once the variables
+    # are combined in a single zarr dataset it won't be clear what these
+    # additional coordinates are referring to.
+    dataset = dataset.reset_coords(drop=True)
+
+    # Downcast lat and lon coordinates to float32. This is because there are
+    # small rounding-error (~1e-14 relative error) discrepancies between the
+    # float64 latitude coordinates across different source NetCDF files, and
+    # xarray_beam complains about this. After downcasting to float32 the
+    # differences go away, and the extra float64 precision isn't important to us.
+    # (Ideally you'd be able to specify a tolerance for the comparison, but this
+    # works for now.)
+    dataset = dataset.assign_coords(
+        latitude=dataset["latitude"].astype(np.float32),
+        longitude=dataset["longitude"].astype(np.float32))
+
+    return dataset
+
+
+def parse_arguments(desc: str) -> t.Tuple[argparse.Namespace, t.List[str]]:
+    parser = argparse.ArgumentParser(description=desc)
+
+    parser.add_argument("--output_path", type=str, required=True,
+                        help="Path to the destination Zarr archive.")
+    parser.add_argument('-s', "--start_date", default='2020-01-01',
+                        help='Start date, iso format string.')
+    parser.add_argument('-e', "--end_date", default='2020-01-02',
+                        help='End date, iso format string.')
+    parser.add_argument("--temp_location", type=str, required=True,
+                        help="A temp location where this data is stored temporarily.")
+    parser.add_argument('--find-missing', action='store_true', default=False,
+                        help='Print all paths to missing input data.')  # implementation pending
+    parser.add_argument("--pressure_levels_group", type=str, default="weatherbench_13",
+                        help="Group label for the set of pressure levels to use.")
+    parser.add_argument("--time_chunk_size", type=int, required=True,
+                        help="Number of 1-hourly timesteps to include in a \
+                        single chunk. Must evenly divide 24.")
+
+    return parser.parse_known_args()
---
+++ src/netcdf_to_zarr.py
@@ -0,0 +1,319 @@
+# pylint: disable=line-too-long
+r"""Create a single Zarr dataset from ERA5 NetCDF files.
+
+While this pipeline is intended as a one off on the full data, it should be
+possible to make some simple edits to the pipeline to append data for
+subsequent years by:
+* In a new location (otherwise xarray beam will delete everything!)
+* Override the template to take into account any new dates.
+* Change "daily_date_iterator" to only iterate over the new data to append (
+  while keeping the original "start_date" so chunk indices are still computed
+  correctly).
+* Move all the generated files into the previous location, overwriting when
+  necessary (e.g. template/static files).
+
+Example usage:
+
+  $ python src/netcdf_to_zarr.py \
+      --output_path="gs://gcp-public-data-arco-era5/ar/$USER-1959-2022-full_37-1h-0p25deg-chunk-1.zarr-v2" \
+      --pressure_levels_group="full_37" \
+      --time_chunk_size=1 \
+      --start_date '1959-01-01' \
+      --end_date '2021-12-31' \
+      --runner DataflowRunner \
+      --project $PROJECT \
+      --region $REGION \
+      --temp_location "gs://$BUCKET/tmp/" \
+      --setup_file ./setup.py \
+      --disk_size_gb 500 \
+      --machine_type m1-ultramem-40 \
+      --no_use_public_ips  \
+      --network=$NETWORK \
+      --subnetwork=regions/$REGION/subnetworks/$SUBNET \
+      --job_name $USER-ar-zarr-full \
+      --number_of_worker_harness_threads 20
+"""
+
+# TODO(alvarosg): Make this pipeline resumable in case of error in the middle
+# of execution.
+
+__author__ = 'Matthew Willson, Alvaro Sanchez, Peter Battaglia, Stephan Hoyer, Stephan Rasp'
+
+import dask
+import fsspec
+import logging
+
+import apache_beam as beam
+import numpy as np
+import pandas as pd
+import typing as t
+import xarray as xa
+import xarray_beam as xb
+
+from arco_era5 import (
+    GCP_DIRECTORY,
+    STATIC_VARIABLES,
+    SINGLE_LEVEL_VARIABLES,
+    MULTILEVEL_VARIABLES,
+    PRESSURE_LEVELS_GROUPS,
+    TIME_RESOLUTION_HOURS,
+    get_var_attrs_dict,
+    read_multilevel_vars,
+    read_single_level_vars,
+    read_static_vars,
+    daily_date_iterator,
+    align_coordinates,
+    parse_arguments
+)
+
+INPUT_PATH = GCP_DIRECTORY
+_HOURS_PER_DAY = 24
+# TODO(alvarosg): Add pressure level chunk size.
+
+
+def _get_pressure_levels_arg(pressure_levels_group: str):
+    return PRESSURE_LEVELS_GROUPS[pressure_levels_group]
+
+
+def make_template(data_path: str, start_date: str, end_date: str, time_chunk_size: int,
+                  pressure_levels_group: str) -> t.Tuple[xa.Dataset, t.Dict[str, int]]:
+    """A lazy template with same dims/coords/vars as our expected results."""
+
+    # Get the variable attributes.
+    var_attrs_dict = get_var_attrs_dict(root_path=data_path)
+
+    # Get some sample multi-level data to get coordinates, only for one var,
+    # so it downloads quickly.
+    logging.info("Downloading one variable of sample data for template.")
+    first_year, first_month, first_day = next(iter(
+        daily_date_iterator(start_date, end_date)))
+    sample_multilevel_vars = align_coordinates(
+        read_multilevel_vars(
+            # Date is irrelevant.
+            first_year,
+            first_month,
+            first_day,
+            root_path=data_path,
+            variables=MULTILEVEL_VARIABLES[:1],
+            pressure_levels=_get_pressure_levels_arg(pressure_levels_group)))
+    logging.info("Finished downloading.")
+
+    lat_size = sample_multilevel_vars.sizes["latitude"]
+    lon_size = sample_multilevel_vars.sizes["longitude"]
+    level_size = sample_multilevel_vars.sizes["level"]
+    assert level_size == len(
+        _get_pressure_levels_arg(pressure_levels_group)
+    ), "Mismatched level sizes"
+
+    # Take the coordinates from the richer, multi-level dataset.
+    coords = dict(sample_multilevel_vars.coords)
+    coords["time"] = pd.date_range(
+        pd.Timestamp(start_date),
+        pd.Timestamp(end_date),
+        freq=pd.DateOffset(hours=TIME_RESOLUTION_HOURS),
+        ).values
+    time_size = len(coords["time"])
+
+    template_dataset = {}
+    for name in STATIC_VARIABLES:
+        template_dataset[name] = xa.Variable(
+            dims=("latitude", "longitude"),
+            data=dask.array.zeros(
+                shape=(lat_size, lon_size),
+                dtype=np.float32),
+            attrs=var_attrs_dict[name])
+
+    for name in SINGLE_LEVEL_VARIABLES:
+        template_dataset[name] = xa.Variable(
+            dims=("time", "latitude", "longitude"),
+            data=dask.array.zeros(
+                shape=(time_size, lat_size, lon_size),
+                chunks=-1,  # Will give chunk info directly to `ChunksToZarr``.
+                dtype=np.float32),
+            attrs=var_attrs_dict[name])
+
+    for name in MULTILEVEL_VARIABLES:
+        template_dataset[name] = xa.Variable(
+            dims=("time", "level", "latitude", "longitude"),
+            data=dask.array.zeros(
+                shape=(time_size, level_size, lat_size, lon_size),
+                chunks=-1,  # Will give chunk info directly to `ChunksToZarr``.
+                dtype=np.float32),
+            attrs=var_attrs_dict[name])
+
+    chunk_sizes = {"time": time_chunk_size}
+    return xa.Dataset(template_dataset, coords=coords), chunk_sizes
+
+
+class LoadTemporalDataForDateDoFn(beam.DoFn):
+    def __init__(self, data_path, start_date, pressure_levels_group):
+        self.data_path = data_path
+        self.start_date = start_date
+        self.pressure_levels_group = pressure_levels_group
+
+    def process(self, args):
+
+        """Loads temporal data for a day, with an xarray_beam key for it.."""
+        year, month, day = args
+        logging.info("Loading NetCDF files for %d-%d-%d", year, month, day)
+
+        try:
+            single_level_vars = read_single_level_vars(
+                year,
+                month,
+                day,
+                variables=SINGLE_LEVEL_VARIABLES,
+                root_path=self.data_path)
+            multilevel_vars = read_multilevel_vars(
+                year,
+                month,
+                day,
+                variables=MULTILEVEL_VARIABLES,
+                pressure_levels=_get_pressure_levels_arg(self.pressure_levels_group),
+                root_path=self.data_path)
+        except BaseException as e:
+            # Make sure we print the date as part of the error for easier debugging
+            # if something goes wrong. Note "from e" will also raise the details of the
+            # original exception.
+            raise Exception(f"Error loading {year}-{month}-{day}") from e
+
+        # It is crucial to actually "load" as otherwise we get a pickle error.
+        single_level_vars = single_level_vars.load()
+        multilevel_vars = multilevel_vars.load()
+
+        dataset = xa.merge([single_level_vars, multilevel_vars])
+        dataset = align_coordinates(dataset)
+        offsets = {"latitude": 0, "longitude": 0, "level": 0,
+                   "time": offset_along_time_axis(self.start_date, year, month, day)}
+        key = xb.Key(offsets, vars=set(dataset.data_vars.keys()))
+        logging.info("Finished loading NetCDF files for %s-%s-%s", year, month, day)
+        yield key, dataset
+        dataset.close()
+
+
+def offset_along_time_axis(start_date: str, year: int, month: int, day: int) -> int:
+    """Offset in indices along the time axis, relative to start of the dataset."""
+    # Note the length of years can vary due to leap years, so the chunk lengths
+    # will not always be the same, and we need to do a proper date calculation
+    # not just multiply by 365*24.
+    time_delta = pd.Timestamp(
+        year=year, month=month, day=day) - pd.Timestamp(start_date)
+    return time_delta.days * _HOURS_PER_DAY // TIME_RESOLUTION_HOURS
+
+
+def load_static_data(args, data_path: str) -> t.Tuple[xb.Key, xa.Dataset]:
+    """Loads all static data, with an xarray_beam key for it.."""
+
+    logging.info("Loading static data")
+
+    dataset = read_static_vars(
+        variables=STATIC_VARIABLES, root_path=data_path)
+
+    logging.info("static data loaded.")
+    # It is crucial to actually "load" as otherwise we get a pickle error.
+    dataset = dataset.load()
+
+    # Technically the static data has a time coordinate, but we don't need it.
+    dataset = dataset.squeeze("time").drop("time")
+    dataset = align_coordinates(dataset)
+
+    offsets = {"latitude": 0, "longitude": 0}
+    key = xb.Key(offsets, vars=set(dataset.data_vars.keys()))
+    logging.info("Finished loading static data")
+    return key, dataset
+
+
+def define_pipeline(
+    root: beam.Pipeline,
+    input_path: str,
+    output_path: str,
+    time_chunk_size: int,
+    start_date: str,
+    end_date: str,
+    pressure_levels_group: str
+) -> t.Tuple[beam.Pipeline, beam.Pipeline]:
+    """Defines a beam pipeline to convert the ERA5 NetCDF files to zarr."""
+
+    template, chunk_sizes = make_template(
+        input_path, start_date, end_date, time_chunk_size, pressure_levels_group)
+
+    # We will create a single `chunks_to_zarr` object, but connect it at the end
+    # of the two pipelines separately. This causes the full transformation to be
+    # fused into a single worker operation, which writes chunks directly to final
+    # location (So long as we provide a template and chose a chunk size that
+    # avoids the need for `xb.ConsolidateChunks`).
+    # This works much faster than:
+    # return ((temporal_variables_chunks, static_variables_chunks)
+    #         | beam.Flatten()
+    #         | chunks_to_zarr)
+    # which persists all data in an intermeddiate location.
+    chunks_to_zarr = xb.ChunksToZarr(
+        output_path,
+        # `xb.make_template` should not be necessary as `template`` is already
+        # a single chunk dask array of zeros, which is what `xb.make_template`
+        # converts it to.
+        template=xb.make_template(template),
+        zarr_chunks=chunk_sizes)
+
+    load_temporal_data_for_date_do_fn = LoadTemporalDataForDateDoFn(
+        data_path=input_path,
+        start_date=start_date,
+        pressure_levels_group=pressure_levels_group
+    )
+    logging.info("Setting up temporal variables.")
+    temporal_variables_chunks = (
+            root
+            | "DayIterator" >> beam.Create(daily_date_iterator(start_date, end_date))
+            | "TemporalDataForDay" >> beam.ParDo(load_temporal_data_for_date_do_fn)
+            | xb.SplitChunks(chunk_sizes)
+            # We can skip the consolidation if we are using a `time_chunk_size` that
+            # evenly divides a day worth of data.
+            # | xb.ConsolidateChunks(chunk_sizes)
+            | "ChunksToZarrTemporal" >> chunks_to_zarr
+    )
+
+    logging.info("Setting up static variables.")
+    static_variables_chunks = (
+            root
+            # This is a single element with no parameters.
+            | "DummySingleElement" >> beam.Create(range(1))
+            | "StaticVariableFromNetCDFFile" >> beam.Map(load_static_data,
+                                                         data_path=input_path)
+            | "ChunksToZarrStatic" >> chunks_to_zarr
+    )
+    logging.info("Finished defining pipeline.")
+    return (temporal_variables_chunks, static_variables_chunks)
+
+
+def main():
+    logging.getLogger().setLevel(logging.INFO)
+    fs = fsspec.filesystem('gcs')
+
+    known_args, pipeline_args = parse_arguments(
+        "Create a Zarr dataset from NetCDF files."
+    )
+    pipeline_args.extend(['--save_main_session', 'True'])
+
+    if fs.exists(known_args.output_path):
+        raise ValueError(f"{known_args.output_path} already exists")
+
+    num_steps_per_day = _HOURS_PER_DAY // TIME_RESOLUTION_HOURS
+    if num_steps_per_day % known_args.time_chunk_size != 0:
+        raise ValueError(
+            f"time_chunk_size {known_args.time_chunk_size} must evenly divide {num_steps_per_day}"
+        )
+
+    with beam.Pipeline(argv=pipeline_args) as root:
+        define_pipeline(
+            root,
+            input_path=INPUT_PATH,
+            output_path=known_args.output_path,
+            start_date=known_args.start_date,
+            end_date=known_args.end_date,
+            time_chunk_size=known_args.time_chunk_size,
+            pressure_levels_group=known_args.pressure_levels_group
+        )
+
+
+if __name__ == "__main__":
+    main()
