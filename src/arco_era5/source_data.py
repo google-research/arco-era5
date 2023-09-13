7fd00e5060ab7fa1ba58d7114504b9bace7d05be
rajveer43 <rajveer.rathod1301@gmail.com>
2023-09-13 15:15:57 +0530

    solved

--- src/arco_era5/source_data.py
+++ src/arco_era5/source_data.py
@@ -255,7 +255,7 @@ def get_var_attrs_dict(root_path=GCP_DIRECTORY):
 
     Example:
         >>> variable_attrs = get_var_attrs_dict()
-    """"""Returns attributes for all variables."""
+    """
     root_path = pathlib.Path(root_path)
 
     # The variable attributes should be independent of the date chosen here
