5a0a7653e19bbc55de4b4f412a2ef5eebaa5d618
dabhicusp <sahildabhi@infocusp.com>
2025-08-28 03:51:32 +0000

    nit changes done.

--- src/model-level-native-vertical-zarr-monthly-executor.py
+++ src/model-level-native-vertical-zarr-monthly-executor.py
@@ -15,6 +15,7 @@
 import datetime
 import logging
 import os
+from typing import List, Tuple
 
 from arco_era5 import get_month_range, run_cloud_job
 
@@ -22,26 +23,28 @@ from arco_era5 import get_month_range, run_cloud_job
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Env Variables
 PROJECT = os.environ.get("PROJECT")
 REGION = os.environ.get("REGION")
 BUCKET = os.environ.get("BUCKET")
 INGESTION_JOB_ID = os.environ.get("INGESTION_JOB_ID")
 ARCO_ERA5_WITH_MODEL_LEVEL_SDK_CONTAINER_IMAGE = os.environ.get("ARCO_ERA5_WITH_MODEL_LEVEL_SDK_CONTAINER_IMAGE")
 
-MODEL_LEVEL_FILE_PATH = '/arco-era5/src/update-model-level-native-vertical-zarr-data.py'
-MODEL_LEVEL_ZARR_PATH = 'gs://gcp-public-data-arco-era5/ar/model-level-1h-0p25deg.zarr-v1'
+MODEL_LEVEL_FILE_PATH = "/arco-era5/src/update-model-level-native-vertical-zarr-data.py"
+MODEL_LEVEL_ZARR_PATH = "gs://gcp-public-data-arco-era5/ar/model-level-1h-0p25deg.zarr-v1"
 
 def generate_args(
-        file_path: str,
-        target_path: str,
-        start_date: str,
-        end_date: str,
-        bucket: str,
-        project: str,
-        region: str,
-        job_name: str
-) -> list:
-    args = [
+    file_path: str,
+    target_path: str,
+    start_date: str,
+    end_date: str,
+    bucket: str,
+    project: str,
+    region: str,
+    job_name: str
+) -> List[str]:
+    """Generate the arguments for the Dataflow job."""
+    return [
         file_path,
         "--output_path", target_path,
         "--start_date", start_date,
@@ -59,31 +62,40 @@ def generate_args(
         "--sdk_container_image", ARCO_ERA5_WITH_MODEL_LEVEL_SDK_CONTAINER_IMAGE,
         "--max_num_workers", "100"
     ]
-    return args
 
-if __name__ == "__main__":
 
+def compute_previous_month_range() -> Tuple[str]:
+    """Compute the first and last day of the previous third month from today."""
     today = datetime.date.today()
     third_prev_month = today - datetime.timedelta(days=2*366/12)
     first_day, last_day = get_month_range(third_prev_month)
+    return first_day.strftime("%Y-%m-%d"), last_day.strftime("%Y-%m-%d")
 
-    first_day = first_day.strftime("%Y-%m-%d")
-    last_day = last_day.strftime("%Y-%m-%d")
 
-    job_name = MODEL_LEVEL_ZARR_PATH.split('/')[-1]
-    job_name = os.path.splitext(job_name)[0]
+def create_job_name(zarr_path: str, start_date: str) -> str:
+    """Create a unique job name based on the Zarr path and start date."""
+    base_name = os.path.splitext(zarr_path.split("/")[-1])[0]
+    return f"zarr-data-ingestion-{base_name}-{start_date[:7]}"
 
-    job_name = (
-        f"zarr-data-ingestion-{job_name}-{first_day[:7]}"
-    )
-    override_args = generate_args(file_path=MODEL_LEVEL_FILE_PATH,
-                                  target_path=MODEL_LEVEL_ZARR_PATH,
-                                  start_date=first_day,
-                                  end_date=last_day,
-                                  bucket=BUCKET,
-                                  project=PROJECT,
-                                  region=REGION,
-                                  job_name=job_name)
 
-    run_cloud_job(PROJECT, REGION, INGESTION_JOB_ID, override_args)
+if __name__ == "__main__":
+    try:
+        first_day, last_day = compute_previous_month_range()
+        job_name = create_job_name(MODEL_LEVEL_ZARR_PATH, first_day)
 
+        override_args = generate_args(
+            file_path=MODEL_LEVEL_FILE_PATH,
+            target_path=MODEL_LEVEL_ZARR_PATH,
+            start_date=first_day,
+            end_date=last_day,
+            bucket=BUCKET,
+            project=PROJECT,
+            region=REGION,
+            job_name=job_name
+        )
+
+        run_cloud_job(PROJECT, REGION, INGESTION_JOB_ID, override_args)
+
+    except Exception as e:
+        logger.error("Error running the job: %s", e, exc_info=True)
+        raise
